cef269dff92d2473bee0778aec8a212dd48363dd
Ester-0 <azalea_@hotmail.com>
2023-10-27 13:03:32 +0200

    OK

--- index.js
+++ index.js
@@ -3,27 +3,57 @@ const keyBoard = document.querySelector(".grid-container");
 const previousOperand = document.querySelector(".previous-operand");
 const currentOperand = document.querySelector(".current-operand");
 
-function operation(event){
+function operation(event) {
 
     const button = event.target;
 
     const operacion = button.getAttribute("data-operator");
 
-    const number = button.getAttribute("data-num");
-    console.log(number)
 
-    const equal = button.getAttribute("data-output");
+    const number = button.getAttribute("data-num");
+   
+
+   const equal = button.getAttribute("data-output");
 
     const del = button.getAttribute("data-delete");
 
+    const reset = button.getAttribute("data-reset");
 
-
-    const currentNumber = currentOperand.textContent += number + operacion;
-
-    previousOperand.textContent = currentNumber;
     
+
     
-    const result = eval(number )
+    if (number) {
+        currentOperand.textContent += button.textContent;
+}
+
+    else if (operacion) {
+         previousOperand.textContent = currentOperand.textContent + operacion;
+
+       currentOperand.textContent = "";
+    }
+ 
+
+    else if (del) {
+        currentOperand.textContent = currentOperand.textContent.slice(0, -1);
+    }
+
+
+    else if (reset) { currentOperand.textContent = ""; previousOperand.textContent = ""; }
+    
+
+    else if (equal) {
+        
+
+
+        const total = eval(previousOperand.textContent + currentOperand.textContent); previousOperand.textContent = "";
+        
+
+        
+        currentOperand.textContent = total;
+
+    }
+
+
 
 }
 
